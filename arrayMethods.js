f1e77918cbc631e158be1045209f9a9e3a18ddb9
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-10-19 12:44:57 +0200

    Corrected question 5

--- arrayMethods.js
+++ arrayMethods.js
@@ -73,27 +73,27 @@ inventors.sort((a, b) => a.year - b.year);
 
 console.log(inventors);
 
-// 5. Sorteer de uitvinders op hoeveel jaren ze geleefd hebben, van langste leven naar kostste leven
+// 5. Sorteer de uitvinders op hoeveel jaren ze geleefd hebben, van langste leven naar kortste leven
 // Verwachte uitkomst:
 // [
-//   { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-//   { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-//   { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-//   { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-//   { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-//   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-//   { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-//   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-//   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-//   { first: 'Thomas', last: 'Edison', year: 1847, passed: 1931 },
-//   { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-//   { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 }
+// { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+// { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+// { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+// { first: 'Thomas', last: 'Edison', year: 1847, passed: 1931 },
+// { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+// { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+// { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+// { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+// { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+// { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+// { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+// { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
 // ]
 
 inventors.sort((a, b) => {
   const yearsLivedA = a.passed - a.year;
   const yearsLivedB = b.passed - b.year;
-  return yearsLivedA - yearsLivedB;
+  return yearsLivedB - yearsLivedA;
 })
 
 console.log(inventors);
